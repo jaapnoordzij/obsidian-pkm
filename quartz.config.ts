37f61e229528586d797e8497d82c12a153a2f11b
Jaap Noordzij <739820+jaapnoordzij@users.noreply.github.com>
2025-12-14 22:25:09 +0100

    Quartz sync: Dec 14, 2025, 10:25 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Personal Know Management",
+    pageTitle: "Personal Knowledge Management",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
